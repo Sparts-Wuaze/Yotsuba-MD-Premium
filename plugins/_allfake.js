241dfa3ee6bb492e83aaa3964c48d4a8800b2b17
Sparts-Wuaze <sparts.wuaze@gmail.com>
2025-11-02 13:19:26 -0400

    Actualizar _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -27,8 +27,8 @@ var correo = 'https://whatsapp.com/channel/0029VbBkjlfLSmbWl3SH6737'
 global.redes = [canal, comunidad, git, github, correo].getRandom()
 
 global.nombre = m.pushName || 'Anónimo'
-global.packsticker = `⊹ 🐬 Deymoon🧠 Club\n↳ Deymoon.club\n\n👹 Info:\n dedeymoon-club.vercel.app/`
-global.packsticker2 = `⊹ 👑Bot:\n⊹ ↳ @${botname}\n\n👑 Usuario:\n⊹ ↳ @${nombre}`
+global.packsticker = `⊹ 🐬 Deymoon🧠 Club\n↳ Deymoon.club\n\n👹 Informações:\n dedeymoon-club.vercel.app/`
+global.packsticker2 = `⊹ 👑Robô:\n⊹ ↳ @${botname}\n\n👑 Usuário:\n⊹ ↳ @${nombre}`
 
 global.fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: '', newsletterName: channelRD.name }, externalAdReply: { title: botname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnail: await (await fetch(icono)).buffer(), sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, mentionedJid: null }}
