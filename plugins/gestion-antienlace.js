ad5f48c81b6101af97e8dcf8bd0c96eb689778cb
Sparts-Wuaze <sparts.wuaze@gmail.com>
2025-11-05 10:51:57 -0400

    Actualizar gestion-antienlace.js

--- plugins/gestion-antienlace.js
+++ plugins/gestion-antienlace.js
@@ -1,1 +1,57 @@
-// REGEX to detect links (http(s), www, .com, .net, .org, .gg, etc.) const linkRegex = /(https?:\/\/\S+|www\.\S+|\b[\w\d\-.]+\.(com|net|org|gg|me|xyz|info|io|edu|gov|biz|dev|shop|live|tv|site|online|app|club|link|store|me dia|cloud|fun|pro|vip|icu|space|top|studio|center|agency|name|today|solutions|website|social|group|chat|capital|cool|company|news|pres s|network|world|help|works|plus|run|systems|zone|tools|tube|team|one|page|games|design|direct|center|events|market|partners|school|services|show|support|technology|tips|trade|ventures|wiki|works|click|email|expert|global|life|money|photo|pics|rocks|tech|web|xyz|io|ai| co|in|to|us|uk|ca|au|de|fr|es|it|jp|ru|br|za|nl|se|no|fi|dk|pl|gr|cz|sk|tr|ro|hu|lt|lv|ee|bg|hr|si|rs|ua|by|md|az|ge|am|kz|kg|uz|tm|tj |mn|kr|cn|tw|hk|mo|vn|th|my|sg|ph|id|lk|bd|np|pk|af|ir|iq|sy|jo|lb|il|ps|eg|ma|dz|tn|ly|sd|et|ss|so|dj|er|ke|tz|ug|rw|bi|mw|zm|zm|mg|z w|ao|cm|cv|cf|td|ne|ng|sn|gm|gw|mr|ml|bf|sl|lr|ci|gh|tg|bj|ng|za|mz |sw|na|bw|ls|sz|km|sc|mu|yt|re|pm|bl|mf|gp|mq|gf|sr|cw|aw|sx|bq|vg| ai | ms | const handler = async (m, { conn, usedPrefix, command, args, isAdmin, isOwner }) => { const chat = global.db.data.chats[m.chat] // Default state: enabled if not exists let isAntiLink = chat && typeof chat.antilink !== 'undefined' ? chat.antilink : true // Command to activate/deactivate antilink (admins/owner only) if (['antilink', 'antilink'].includes(command)) { if (!(isAdmin || isOwner)) { return conn.reply(m.chat, '🤨 Only administrators can activate or deactivate antilink.', m, rcanal) } if (args[0] === 'on' || args[0] === 'enable') { if (isAntiLink) return conn.reply(m.chat, `💜 The function *${command}* was already *activated*.`, m, rcanal) isAntiLink = true } else if (args[0] === 'off' || args[0] === 'disable') { if (!isAntiLink) return conn.reply(m.chat, `👑 The *${command}* function was already *disabled*.`, m, rcanal) isAntiLink = false } else { return conn.reply( m.chat, `👑 Admins can enable or disable the *${command}* function using:\n\n💜 *${command}* enable\n💜 *${command}* disable\n\n🛠 Current state » *${isEnable ? '✓ Enabled' : '✗ Disabled'}*`, m, rcanal ) } chat.antilink = isAntiLink return conn.reply(m.chat, `👑 The *${command}* function was *${isAntiLink ? 'enabled' : 'disabled'}* for this group.`, m, rcanal) } } // Detect links and delete message if anti-link is enabled export async function detectLink(conn, m) { const chat = global.db.data.chats[m.chat] const isAntiLink = chat && typeof chat.antienlace !== 'undefined' ? chat.antilink: true // If not enabled or is admin/owner, ignore if (!isAntiLink) return if (m.isGroup && (m.isAdmin || m.isOwner)) return // If the message contains a link if (linkRegex.test(m.text)) { // Reply first await conn.sendMessage(m.chat, { text: '🤨 Don't send a link if you don't want me to delete you.' }, { quoted: m }) // Delete the original message await conn.sendMessage(m.chat, { delete: m.key }) } } handler.help = ['antilink', 'antilink'] handler.tags = ['group'] handler.command = ['antilink', 'antilink'] handler.group = true export default handler 
+// REGEX para detectar enlaces (http(s), www, .com, .net, .org, .gg, etc.)
+const linkRegex = /(https?:\/\/\S+|www\.\S+|\b[\w\d\-.]+\.(com|net|org|gg|me|xyz|info|io|edu|gov|biz|dev|shop|live|tv|site|online|app|club|link|store|media|cloud|fun|pro|vip|icu|space|top|studio|center|agency|name|today|solutions|website|social|group|chat|capital|cool|company|news|press|network|world|help|works|plus|run|systems|zone|tools|tube|team|one|page|games|design|direct|center|events|market|partners|school|services|show|support|technology|tips|trade|ventures|wiki|works|click|email|expert|global|life|money|photo|pics|rocks|tech|web|xyz|io|ai|co|in|to|us|uk|ca|au|de|fr|es|it|jp|ru|br|za|nl|se|no|fi|dk|pl|gr|cz|sk|tr|ro|hu|lt|lv|ee|bg|hr|si|rs|ua|by|md|az|ge|am|kz|kg|uz|tm|tj|mn|kr|cn|tw|hk|mo|vn|th|my|sg|ph|id|lk|bd|np|pk|af|ir|iq|sy|jo|lb|il|ps|eg|ma|dz|tn|ly|sd|et|ss|so|dj|er|ke|tz|ug|rw|bi|mw|zm|zm|mg|zw|ao|cm|cv|cf|td|ne|ng|sn|gm|gw|mr|ml|bf|sl|lr|ci|gh|tg|bj|ng|za|mz|sw|na|bw|ls|sz|km|sc|mu|yt|re|pm|bl|mf|gp|mq|gf|sr|cw|aw|sx|bq|vg|ai|ms|bm|tc|ky|jm|bs|bb|lc|vc|gd|dm|ag|kn|vi|pr|do|ht|cu|bs|us|ca|mx|gt|bz|sv|hn|ni|cr|pa|co|ec|pe|bo|py|uy|ar|cl|br|gy|sr|gf|fk)\b)/gi
+
+const handler = async (m, { conn, usedPrefix, command, args, isAdmin, isOwner }) => {
+  const chat = global.db.data.chats[m.chat]
+  // Estado por defecto: activado si no existe
+  let isAntiLink = chat && typeof chat.antienlace !== 'undefined' ? chat.antienlace : true
+
+  // Comando para activar/desactivar antienlace (solo admins/owner)
+  if (['antienlace', 'antilink'].includes(command)) {
+    if (!(isAdmin || isOwner)) {
+      return conn.reply(m.chat, '🤨 Only administrators can activate or deactivate the anti-link.', m, rcanal)
+    }
+    if (args[0] === 'on' || args[0] === 'enable') {
+      if (isAntiLink) return conn.reply(m.chat, `💜 The function *${command}* was already *activated*.`, m, rcanal)
+      isAntiLink = true
+    } else if (args[0] === 'off' || args[0] === 'disable') {
+      if (!isAntiLink) return conn.reply(m.chat, `👑 The function *${command}* was already *disabled*.`, m, rcanal)
+      isAntiLink = false
+    } else {
+      return conn.reply(
+        m.chat,
+        `👑 Admins can enable or disable the *${command}* function using:\n\n💜 *${command}* enable\n💜 *${command}* disable\n\n🛠 Current state » *${isEnable ? '✓ Enabled' : '✗ Disabled'}*`,
+        m, rcanal
+      )
+    }
+    chat.antienlace = isAntiLink
+    return conn.reply(m.chat, `👑 The function *${command}* was *${isAntiLink ? 'activated' : 'deactivated'}* for this group.`, m, rcanal)
+  }
+}
+
+// Detectar enlaces y eliminar mensaje si antienlace está activado
+export async function detectLink(conn, m) {
+  const chat = global.db.data.chats[m.chat]
+  const isAntiLink = chat && typeof chat.antienlace !== 'undefined' ? chat.antienlace : true
+
+  // Si no está activado o es admin/owner, ignorar
+  if (!isAntiLink) return
+  if (m.isGroup && (m.isAdmin || m.isOwner)) return
+
+  // Si el mensaje contiene un enlace
+  if (linkRegex.test(m.text)) {
+    // Responder primero
+    await conn.sendMessage(m.chat, { text: '🤨 Don't send the link if you don't want me to delete you.' }, { quoted: m })
+    // Borrar el mensaje original
+    await conn.sendMessage(m.chat, {
+      delete: m.key
+    })
+  }
+}
+
+handler.help = ['antienlace', 'antilink']
+handler.tags = ['group']
+handler.command = ['antienlace', 'antilink']
+handler.group = true
+
+export default handler
