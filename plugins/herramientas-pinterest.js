e253f7e2e420eadaebedffbd3b153e8ed3387008
Sparts-Wuaze <sparts.wuaze@gmail.com>
2025-11-05 10:47:17 -0400

    Actualizar herramientas-pinterest.js

--- plugins/herramientas-pinterest.js
+++ plugins/herramientas-pinterest.js
@@ -3,7 +3,7 @@ import baileys from '@whiskeysockets/baileys'
 import cheerio from 'cheerio'
 
 let handler = async (m, { conn, text, args, usedPrefix }) => {
-if (!text) return m.reply(`*ᐛ👑* ¿Dime que imagen de pinterest quieres que dubuje?`)
+if (!text) return m.reply(`*ᐛ👑* Tell me which Pinterest image you want me to draw?`)
 try {
 await m.react('🕒')
 if (text.includes("https://")) {
@@ -13,11 +13,11 @@ await conn.sendMessage(m.chat, { [isVideo ? "video" : "image"]: { url: i.downloa
 } else {
 const results = await pins(text)
 if (!results.length) {
-return conn.reply(m.chat, `*ᐛ👑* No tengo la capacidad paea dibujar lo que pediste`, m, rcanal)
+return conn.reply(m.chat, `*ᐛ👑* I don't have the ability to draw what you asked for.`, m, rcanal)
 }
 const medias = results.slice(0, 10).map(img => ({ type: 'image', data: { url: img.image_large_url } }))
 await conn.sendSylphy(m.chat, medias, {
-caption: `*ᐛ👑* Pude hacer ${medias.length} de ${text}`, quoted: m })
+caption: `*ᐛ👑* I was able to do ${medias.length} the ${text}`, quoted: m })
 await m.react('💟')
 }} catch (e) {
 await m.react('✖️')
