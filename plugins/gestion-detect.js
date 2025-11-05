10097a1d3e7a5f529b35a9a03fb43b6bfc1dd1b9
Sparts-Wuaze <sparts.wuaze@gmail.com>
2025-11-05 10:54:01 -0400

    Actualizar gestion-detect.js

--- plugins/gestion-detect.js
+++ plugins/gestion-detect.js
@@ -17,13 +17,13 @@ const usuario = await resolveLidToRealJid(m?.sender, conn, m?.chat)
 const groupAdmins = participants.filter(p => p.admin)
 const rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: '', newsletterName: channelRD.name }, externalAdReply: { title: "Yotsuba Nakano IA", body: textbot, mediaUrl: null, description: null, previewType: "PHOTO", thumbnail: await (await fetch(icono)).buffer(), sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, mentionedJid: null }}
 const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || 'https://files.catbox.moe/xr2m6u.jpg'
-const nombre = `> Hola chicos`
-const foto = `> Hola chicos`
-const edit = `> Hola chicos`
-const newlink = `> Hola chicos`
-const status = `> Hola chicos`
-const admingp = `💜 Aviso:\n\n> @${users.split('@')[0]} *Ahora es administrador de este grupo* ya que *@${usuario.split('@')[0]},* Le acaba de dar privilegios de administración.`
-const noadmingp = `💜 Aviso:\n\n> @${users.split('@')[0]} *Ya no es administrador de este grupo* ya que *@${usuario.split('@')[0]},* Le quito los privilegios.`
+const nombre = `> Hello chicos`
+const foto = `> Hello chicos`
+const edit = `> Hello chicos`
+const newlink = `> Hello chicos`
+const status = `> Hello chicos`
+const admingp = `💜 Notice:\n\n> @${users.split('@')[0]} *You are now an administrator of this group* because *@${user.split('@')[0]},* has just given you administrative privileges.`
+const noadmingp = `💜 Notice:\n\n> @${users.split('@')[0]} *I am no longer an administrator of this group* because *@${user.split('@')[0]},* removed their privileges.`
 if (chat.detect && m.messageStubType == 2) {
 const uniqid = (m.isGroup ? m.chat : m.sender).split('@')[0]
 const sessionPath = `./${sessions}/`
